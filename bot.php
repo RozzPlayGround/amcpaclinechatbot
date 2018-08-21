7ddd8cf17b5ad09ddc9a587583bea7fa63215b28
Peerayot P <nong_za9779@hotmail.com>
2018-08-21 15:22:51 +0700

    update

--- bot.php
+++ bot.php
@@ -1,4 +1,9 @@
 <?php
+
+$messages = ['type' => 'text','text' => 'Hi ja'];
+$data = ['replyToken' => '123','messages' => [$messages],];
+echo $data;
+
 $access_token = 'iZoNnaTJ1SersZhobYnkptT1Vk2y1dTe4y9vT/LBPIf5WrhDJVobFBw7k0UPWcW8rp1pvCWI4L+TF78PQfn86b9/xRB8ci86Edcrqp73Kzfp7q7Mg2C4JTO8s2jiNBmymOuVa9/c14CQEZTGxloWOQdB04t89/1O/w1cDnyilFU=';// Get POST body content
 $content = file_get_contents('php://input');// Parse JSON
 $events = json_decode($content, true);// Validate parsed JSON data
@@ -9,7 +14,7 @@ if (!is_null($events['events'])) {	// Loop through each event
 			$replyToken = $event['replyToken'];			// Build message to reply back			
 			$messages = ['type' => 'text','text' => $text];			// Make a POST Request to Messaging API to reply to sender			
 			$url = 'https://api.line.me/v2/bot/message/reply';			
-			$data = ['replyToken' => $replyToken,'messages' => $messages];			
+			$data = ['replyToken' => $replyToken,'messages' => [$messages],];			
 			$post = json_encode($data);			
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);			
 			$ch = curl_init($url);			
