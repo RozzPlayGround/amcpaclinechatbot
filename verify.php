e90ae07c611b93200559fd57ee85c2b21e1944a8
Peerayot P <nong_za9779@hotmail.com>
2018-08-21 17:36:40 +0700

    update

--- verify.php
+++ verify.php
@@ -7,7 +7,7 @@ $headers = array('Content-Type: application/x-www-form-urlencoded');
 
 $payload = json_encode( array("access_token" => $access_token) );
 
-$data_string = json_encode($data);    
+ 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
